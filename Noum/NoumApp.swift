27534d8cf9787901ab1a78d30fc88e66e831bcf8
Jordan <45615836+JordanCdev@users.noreply.github.com>
2025-01-25 00:00:19 +0000

    Initial Commit

---
+++ Noum/NoumApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoumApp.swift
+//  Noum
+//
+//  Created by Jordan Coaten on 25/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct NoumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
